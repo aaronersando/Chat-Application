14150714df838a0d7e37dae732bc9168f00f0c1d
aaronersando <agersando@gmail.com>
2025-04-09 22:15:37 +0800

    Chat Controller and chatMessage class data configured

---
+++ application/src/main/java/com/chat/application/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.chat.application.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.chat.application.model.ChatMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message) {
+        // Logic to send the message to the chat room
+        return message;
+    }
+
+    @GetMapping("chat")
+    public String chat() {
+        return "chat";
+    }
+    
+
+}
